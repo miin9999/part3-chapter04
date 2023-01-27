e0ad513ec579f1d1918e8f0d1cb11c1d03527562
miin <busster@naver.com>
2023-01-28 06:20:42 +0900

    a

--- app/src/main/java/fastcampus/aop_part3_chapter04/MainActivity.kt
+++ app/src/main/java/fastcampus/aop_part3_chapter04/MainActivity.kt
@@ -3,7 +3,10 @@ package fastcampus.aop_part3_chapter04
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import fastcampus.aop_part3_chapter04.adapter.BookAdapter
 import fastcampus.aop_part3_chapter04.api.BookAPI
+import fastcampus.aop_part3_chapter04.databinding.ActivityMainBinding
 import fastcampus.aop_part3_chapter04.model.SearchBooksDto
 import retrofit2.Call
 import retrofit2.Callback
@@ -12,10 +15,22 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var adapter : BookAdapter
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+
+
+        setContentView(binding.root)
+        initbookRecyclerView()
+
+
 
         val retrofit = Retrofit.Builder()
             .baseUrl("https://openapi.naver.com/")
@@ -25,7 +40,7 @@ class MainActivity : AppCompatActivity() {
         val bookService = retrofit.create(BookAPI::class.java)
 
         bookService.getBooksByName("EE29yHWSCmCGhREiVgco","DH6VFtsHdD",
-        "ddd")
+        "toeic")
             .enqueue(object : Callback<SearchBooksDto>{
                 override fun onResponse(
                     call: Call<SearchBooksDto>,
@@ -40,21 +55,27 @@ class MainActivity : AppCompatActivity() {
 
                         it.books.forEach { book ->
                             Log.d(TAG,book.toString())
-
                         }
+                        adapter.submitList(it.books) // submitList : 리스트가 이것으로 체인지됨
+                        // BookAdapter 안에서 currentList가 book으로 대체된다는 뜻
+
                     }
                 }
-
                 override fun onFailure(call: Call<SearchBooksDto>, t: Throwable) {
 
                     Log.e(TAG,t.toString()) // 실패시 t로 들어옴
                 }
-
             })
 
 
     }
 
+
+    fun initbookRecyclerView(){
+        adapter = BookAdapter()
+        binding.bookRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.bookRecyclerView.adapter = adapter
+    }
 
     companion object {
         private const val TAG = "MainActivity"
